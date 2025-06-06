647bf48a1553b68dc29194cf0fc07d247394a68d
Efrain Colmenares Guerrero <efraxpc@gmail.com>
2025-06-05 22:19:41 -0300

    added email consumer

---
+++ src/queues/email.consumer.ts
@@ -0,0 +1,29 @@
+import { config } from '@notifications/config';
+import { winstonLogger } from '@efraxpc/jobber-shared';
+import { Channel, ConsumeMessage } from 'amqplib';
+import { Logger } from 'winston';
+import { createConnection } from '@notifications/queues/connection';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'emailConsumer', 'debug');
+
+async function consumeAuthEmailMessages(channel: Channel): Promise<void> {
+  try {
+    if (!channel) {
+      channel = await createConnection() as Channel;
+    }
+    const exchangeName = 'jobber-email-notification';
+    const routingKey = 'auth-email';
+    const queueName = 'auth-email-queue';
+    await channel.assertExchange(exchangeName, 'direct');
+    const jobberQueue = await channel.assertQueue(queueName, { durable: true, autoDelete: false });
+    await channel.bindQueue(jobberQueue.queue, exchangeName, routingKey);
+    channel.consume(jobberQueue.queue, async (msg: ConsumeMessage | null) => {
+        //send emails
+        //aknowledge
+    });
+  } catch (error) {
+    log.log('error', 'NotificationService EmailConsumer consumeAuthEmailMessages() method error:', error);
+  }
+}
+
+export { consumeAuthEmailMessages };
